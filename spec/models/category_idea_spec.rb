8c949b2ba399e2f2eadcf74dece30926527da489
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2021-02-27 00:07:29 +0900

    単体テストコードの記述

--- spec/models/category_idea_spec.rb
+++ spec/models/category_idea_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CategoryIdea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アイデアの保存' do
+    before do
+      @category_idea = FactoryBot.build(:category_idea)
+    end
+
+    it 'name,bodyが正しく入力されていれば保存できること' do
+      expect(@category_idea).to be_valid
+    end
+
+    it 'nameが空だと保存できないこと' do
+      @category_idea.name = ''
+      @category_idea.valid?
+      expect(@category_idea.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'bodyが空だと保存できないこと' do
+      @category_idea.body = ''
+      @category_idea.valid?
+      expect(@category_idea.errors.full_messages).to include("Body can't be blank")
+    end
+  end
 end
+
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'アイデアの保存' do
+    before do
+      @category = FactoryBot.build(:category)
+    end
+
+    it 'nameが正しく入力されていれば保存できること' do
+      expect(@category).to be_valid
+    end
+
+    it 'nameに一意性がないと保存できないこと' do
+      @category.save
+      another_category = FactoryBot.build(:category)
+      another_category.valid?
+      expect(another_category.errors.full_messages).to include("Name has already been taken")
+    end
+  end
+end
